5d25e0aa8058579f71ee3c964de5bf5183e88103
Jaeha Lee <jaehaaheaj@gmail.com>
2021-06-22 21:02:01 +0900

    boj 13977 better solution

--- BOJ/py/13977.py
+++ BOJ/py/13977.py
@@ -4,7 +4,9 @@ input=sys.stdin.readline
 mod=10**9+7
 m=4*10**6
 
-def p(a,b):return 1 if b==0 else ((p(a,b//2)**2)*(a if b%2 else 1))%mod
+#def p(a,b):return 1 if b==0 else ((p(a,b//2)**2)*(a if b%2 else 1))%mod
+# use python pow instead.
+# this impl was about 10 times slower on test sets.
 
 f=[1 for _ in range(m+1)]
 for i in range(2,m+1): f[i]=f[i-1]*i%mod
@@ -12,6 +14,6 @@ fi=[-1 for _ in range(m+1)]
 
 for _ in range(int(input())):
     n,k=map(int,input().split())
-    if fi[n-k]==-1:fi[n-k]=p(f[n-k],mod-2)%mod
-    if fi[k]==-1:fi[k]=p(f[k],mod-2)%mod
+    if fi[n-k]==-1:fi[n-k]=pow(f[n-k],mod-2,mod)
+    if fi[k]==-1:fi[k]=pow(f[k],mod-2,mod)
     print(f[n]*(fi[n-k]*fi[k]%mod)%mod)
