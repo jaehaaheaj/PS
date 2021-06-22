f9a768d4e19ab8c67e1c656c5f78947727deaaa9
Jaeha Lee <jaehaaheaj@gmail.com>
2021-06-22 21:30:44 +0900

    better impl for factorial modulo in python

--- tools/factorial_modulo/factorial_modulo.py
+++ tools/factorial_modulo/factorial_modulo.py
@@ -7,9 +7,10 @@ f=[1 for _ in range(size+1)]
 for i in range(2,size+1): f[i]=f[i-1]*i%mod
 
 # p: calc power(a,n) in O(log n)
-def p(a,b):return 1 if b==0 else ((p(a,b//2)**2)*(a if b%2 else 1))%mod
+#def p(a,b):return 1 if b==0 else ((p(a,b//2)**2)*(a if b%2 else 1))%mod
+# but, python's internal implementation is stronger.
 
 # use Fermat's little theorem to get
 # 'modular multiplicative inverse' of n!
 n = 3
-print(p(f[n],mod-2)%mod)
+print(pow(f[n],mod-2,mod))
